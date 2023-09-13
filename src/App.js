5728b3ea14adc0bae5f81f11e65df95931ca261a
Mubeen Patel <mubeen13684@mastek.com>
2023-09-13 17:48:07 +0530

    add all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
+import Header from './components/Header/Header';
+import ResultTable from './components/ResultsTable/ResultsTable';
+import UserInput from './components/UserInput/UserInput';
 
 function App() {
+  const [userInput, setUserInput]= useState(null)
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput)
+  };
+
+    const yearlyData = []; 
+    if(userInput){
+      let currentSavings = +userInput['current-savings']; 
+    const yearlyContribution = +userInput['yearly-contribution']; 
+    const expectedReturn = +userInput['expected-return'] / 100;
+    const duration = +userInput['duration'];
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+    }
+
+    
+
+    // do something with yearlyData ...
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <UserInput onCalculate={calculateHandler} />
+      {!userInput && <p style={{textAlign:'center'}}>No Investment calculated yet</p>}
+      {userInput && <ResultTable data={yearlyData} initialInvestment={userInput['current-savings']} /> }
+           
     </div>
   );
 }
